e39c914310c01c9794ba1ce4c10b9c5f9d44461f
ralfoide <ralfoide@7d5ca678-6032-0410-b872-3f9e62d19b3b>
2007-06-08 19:51:40 +0000

    Added account commnad line option. Added support for multiple source dirs. Skip already downloaded files.

--- trunk/gmup.py
+++ trunk/gmup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
-# vim: ts=2 sw=2
+# vim: ts=2 sw=2 et
+# Note: this is equivalent to:
+# vim: tabstop=2 shiftwidth=2 expandtab
 #
 # Original:
 # sendmsg.py -- Demo to send a message via Gmail using libgmail
@@ -27,6 +29,7 @@ DRY_RUN = False
 # Allow us to run using installed `libgmail` or the one in a "libgmail" subdir.
 try:
   import libgmail
+  import lgconstants
   ## Wouldn't this the preffered way?
   ## We shouldn't raise a warning about a normal import
   ##logging.warn("Note: Using currently installed `libgmail` version.")
@@ -36,6 +39,7 @@ except ImportError:
                   os.path.realpath(os.path.join(os.path.dirname(__file__),
                                                 "libgmail")))
   import libgmail
+  import lgconstants
 
 def verbose(msg, *args):
   global VERBOSE
@@ -44,8 +48,9 @@ def verbose(msg, *args):
 
 def usage_and_exit():  
   print """
-Usage: %s [-pvnh] <account> <base_dir>
+Usage: %s [-apvnh] <dir1> .. <dirN>
 Options:
+  -a, --account=name  Account name
   -p, --password=pwd  Account password
   -v, --verbose       Verbose mode
   -n, --dry-run       Dry-run
@@ -55,35 +60,40 @@ Options:
 
 def get_params():
   pw = None
+  account = None
   optlist, args = getopt.gnu_getopt(sys.argv[1:],
-                                    shortopts="p:vnhdl:",
-                                    longopts=["password=",
+                                    shortopts="p:a:vnhdl:",
+                                    longopts=["account=",
+                                              "password=",
                                               "verbose",
                                               "dry-run",
                                               "help",
 																							"debug",
 																							"limit="])
   for o, a in optlist:
+    global VERBOSE, DEBUG, DRY_RUN, NLIMIT
     if o in ["-v", "--verbose"]:
-      global VERBOSE
       VERBOSE = True
     elif o in ["-n", "--dry-run"]:
-      global DRY_RUN
       DRY_RUN = True
     elif o in ["-p", "--password"]:
       pw = a
+    elif o in ["-a", "--account"]:
+      account = a
     elif o in ["-h", "--help"]:
       usage_and_exit()
     elif o in ["-d", "--debug"]:
-      global DEBUG
       DEBUG = True
+      VERBOSE = True
     elif o in ["-l", "--limit"]:
       global NLIMIT
       NLIMIT = int(a)
   
+  if not account:
+    usage_and_exit()
+
   try:
-    account = args[0]
-    root_dir = args[1]
+    root_dirs = args
   except IndexError:
     usage_and_exit()
   
@@ -91,7 +101,7 @@ def get_params():
     from getpass import getpass
     pw = getpass("Password for %s: " % account)
   
-  return account, pw, root_dir
+  return account, pw, root_dirs
 
 def connect(name, pw):
   ga = libgmail.GmailAccount(name, pw)
@@ -105,25 +115,34 @@ def connect(name, pw):
     verbose("Login failed. (Wrong username/password?)")
   return None
 
-def get_files(initial_dir):
+def get_files(initial_dirs):
   result = []
-  for root_dir, dirs, files in os.walk(initial_dir):
-    for file in files:
-      if file.endswith(".jpg"):
-        full = os.path.join(root_dir, file)
-        result.append(full)
+  for initial_dir in initial_dirs:
+    for root_dir, dirs, files in os.walk(initial_dir):
+      for file in files:
+        if file.endswith(".jpg"):
+          full = os.path.join(root_dir, file)
+          result.append(pretty_filename(full, initial_dir))
   verbose("Found %d files" % len(result))
   return result
 
-def process_files(ga, files, root_dir):
+def pretty_filename(full_path, root_dir):
+  fname = full_path
+  if fname.startswith(root_dir):
+    fname = fname[len(root_dir):]
+  if fname.startswith("/"):
+    fname = fname[1:]
+  return (full_path, fname)
+
+def process_files(ga, files, existing):
   n = len(files)
   for i in xrange(n):
-    full = files[i]
-    fname = full
-    if fname.startswith(root_dir):
-      fname = fname[len(root_dir):]
-    if fname.startswith("/"):
-      fname = fname[1:]
+    full, fname = files[i]
+
+    if fname in existing:
+      verbose("Skipping '%s' (%d of %d, %.2f%%)", fname, i + 1, n, 100.0 * i / n)
+      continue
+
     subject = fname
     to = "%s@gmail.com" % ga.name
     size = os.stat(full)[ST_SIZE]
@@ -132,9 +151,9 @@ def process_files(ga, files, root_dir):
     digest = s.hexdigest()
     f.close()
     msg = """
-PATH{%s}
-SIZE{%s}
-SHA{%s}
+PATH: \"%s\"
+SIZE: %s
+SHA: %s
 """ % (fname, size, digest)
     gmsg = libgmail.GmailComposedMessage(to, subject, msg,
                                          filenames=[full])
@@ -144,11 +163,10 @@ SHA{%s}
     if DEBUG:
       verbose("# Gmail Message: %s", repr(gmsg.__dict__))
 
-    global DRY_RUN
+    global DRY_RUN, NLIMIT
     if not DRY_RUN:
       if ga.sendMessage(gmsg):
         verbose("Message sent `%s` successfully." % subject)
-        global NLIMIT
         if NLIMIT > 0:
           NLIMIT -= 1
           if NLIMIT == 0:
@@ -156,13 +174,24 @@ SHA{%s}
       else:
         verbose("Could not send message.")
 
+def get_existing_files(ga):
+  verbose("Retrieving list of existing files...")
+  threads = ga.getMessagesByLabel(lgconstants.U_INBOX_SEARCH)
+  files = {}
+  for thread in threads:
+    for msg in thread:
+      files[msg.subject] = msg
+  verbose("Retrieved %d messages", len(files))
+  return files
+
 def main():
-  account, password, root_dir = get_params()
-  files = get_files(root_dir)
+  account, password, root_dirs = get_params()
+  files = get_files(root_dirs)
   if files:
     ga = connect(account, password)
     if ga:
-      process_files(ga, files, root_dir)
+      existing = get_existing_files(ga)
+      process_files(ga, files, existing)
   verbose("Done.")
   
 if __name__ == "__main__":
